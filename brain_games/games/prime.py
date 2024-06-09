b377af6473faec6f6e096b53cfe8f2423e21ec6b
Polina Kurchenko <joohndee@mail.ru>
2024-06-10 02:42:18 +0300

    corr brain prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,34 +1,16 @@
-import prompt
 from random import randint
 import math
 
-
 def is_prime():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    index = 0
-    while index < 3:
-        num = randint(0, 100)
-        print(f'Question: {num}')
-        a = prompt.string('Your answer: ')
-        if num <= 1:
-            r = 'no'
-        else:
-            r = 'yes'
-            number_sqrt = int(math.sqrt(num))
-            divisors = range(2, (number_sqrt + 1))
-            for element in divisors:
-                if num % element == 0:
-                    r = 'no'
-        if a.lower() != r:
-            srry = print(f"'{a}' is wrong answer ;(. Correct answer was '{r}'.")
-            srry2 = print(f"Let's try again, {name}!")
-            return srry, srry2
-            break
-        else:
-            print('Correct!')
-            index = index + 1
-    congrats = print(f'Congratulations, {name}!')
-    return congrats
+    num = randint(0, 100)
+    question = f'{num}'
+    if num <= 1:
+        r = 'no'
+    else:
+        r = 'yes'
+        number_sqrt = int(math.sqrt(num))
+        divisors = range(2, (number_sqrt + 1))
+        for element in divisors:
+            if num % element == 0:
+                r = 'no'
+    return question, r
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -2,10 +2,29 @@
 
 
 from brain_games.games.prime import is_prime
-
+import prompt
 
 def main():
-    is_prime()
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    index = 0
+    while index < 3:
+        question, r = is_prime()
+        print(f'Question: {question}')
+        a = prompt.string('Your answer: ')
+        if a.lower() != r:
+            srry = f"'{a}' is wrong answer ;(. Correct answer was '{r}'."
+            srry2 = f"Let's try again, {name}!"
+            print(srry)
+            print(srry2)
+            return
+        else:
+            print('Correct!')
+            index = index + 1
+    congrats = print(f'Congratulations, {name}!')
+    return congrats
 
 
 if __name__ == '__main__':
