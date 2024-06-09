c8d575d30cb2a47a23728fb94673539a9dc3c2c6
Polina Kurchenko <joohndee@mail.ru>
2024-06-10 01:19:24 +0300

    corr brain calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,33 +1,16 @@
-import prompt
 from random import randint, choice
 
 
 def calc():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('What is the result of the expression')
-    index = 0
-    while index < 3:
-        operators = ['+', '-', '*']
-        num1 = randint(1, 100)
-        num2 = randint(1, 100)
-        operator = choice(operators)
-        if operator == '+':
-            r = num1 + num2
-        elif operator == '-':
-            r = num1 - num2
-        elif operator == '*':
-            r = num1 * num2
-        print(f'Question: {num1} {operator} {num2}')
-        a = prompt.integer('Your answer: ')
-        if a != r:
-            srry = print(f"'{a}' is wrong answer ;(. Correct answer was '{r}'.")
-            srry2 = print(f"Let's try again, {name}!")
-            return srry, srry2
-            break
-        else:
-            print('Correct!')
-            index = index + 1
-    congrats = print(f'Congratulations, {name}!')
-    return congrats
+    operators = ['+', '-', '*']
+    num1 = randint(1, 100)
+    num2 = randint(1, 100)
+    operator = choice(operators)
+    if operator == '+':
+        r = num1 + num2
+    elif operator == '-':
+        r = num1 - num2
+    elif operator == '*':
+        r = num1 * num2
+    question = f'{num1} {operator} {num2}'
+    return question, r 
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,10 +2,30 @@
 
 
 from brain_games.games.calc import calc
+import prompt
 
 
 def main():
-    calc()
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('What is the result of the expression')
+    index = 0
+    while index < 3:
+        question, r = calc()
+        print(f'Question: {question}')
+        a = prompt.integer('Your answer: ')
+        if a != r:
+            srry = f"'{a}' is wrong answer ;(. Correct answer was '{r}'."
+            srry2 = f"Let's try again, {name}!"
+            print(srry)
+            print(srry2)
+            return
+        else:
+            print('Correct!')
+            index = index + 1
+    congrats = print(f'Congratulations, {name}!')
+    return congrats
 
 
 if __name__ == '__main__':
